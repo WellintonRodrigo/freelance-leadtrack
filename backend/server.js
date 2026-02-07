3495d7a28546d43debbcb9453f68f12bba8f9d50
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-02-07 00:34:29 -0300

    fix: corrige sintaxe da validação e ajusta status code para 400

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const Joi= require('joi')
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -21,11 +22,20 @@ app.get('/leads', (req, res) => {
 });
 // Rota 2: Receber novo lead (O Formulário vai usar essa)
 app.post('/leads', (req, res)=>{
-    const {nome, email, whatsapp}= req.body;
+
     // Validação
-    if(!nome || !email || !whatsapp){
-    return res.status(400).json({error:"Todos os campos são obrigatórios."});
- }
+   const schema = Joi.object({
+        nome: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        whatsapp: Joi.string().min(10).required()
+   });
+
+const {error} = schema.validate(req.body);
+
+if(error){
+    return res.status(404).json({error: error.details[0].message});
+}
+const {nome, email, whatsapp}=req.body;
 
 const novoLead = {
         id: leads.length + 1,
@@ -38,7 +48,40 @@ const novoLead = {
     leads.push(novoLead);
     console.log("Novo lead recebido:", novoLead);
     res.status(201).json(novoLead);
-});   
+});
+
+// Rota 3: Editar Status do Lead
+
+app.patch('/leads/:id', (req, res)=>{
+    const {id} = req.params
+    const {status} = req.body
+
+    const leadIndex =leads.findIndex(l=>l.id===parseInt(id))
+
+    if(leadIndex === -1){
+        return res.status(404).json({ error: "Lead não encontrado"});
+    }
+    // Atualiza apenas o status
+    leads[leadIndex].status = status || leads[leadIndex].status;
+
+    console.log(`Lead ${id} atualizado para: ${leads[leadIndex].status}`);
+    res.json(leads[leadIndex]);
+});
+
+// Rota 4: Deletar Lead
+app.delete('/leads/:id', (req, res) => {
+    const { id } = req.params;
+    const initialLength = leads.length;
+    
+    leads = leads.filter(l => l.id !== parseInt(id));
+
+    if (leads.length === initialLength) {
+        return res.status(404).json({ error: "Lead não encontrado para exclusão." });
+    }
+
+    console.log(`Lead ${id} removido.`);
+    res.status(204).send(); // 204 significa "Sucesso, mas sem conteúdo para retornar"
+});
 
 // Porta do servidor
 const PORT = process.env.PORT || 5000;

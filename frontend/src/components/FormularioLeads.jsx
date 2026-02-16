75bc389ba61aa28e5c1888322f3a425534dac9e1
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-02-16 11:25:24 -0300

    feat: adiciona máscara de formatação no campo de WhatsApp
    Implementada função de regex para formatar o input de telefone no padrão (XX) XXXXX-XXXX em tempo real.

--- frontend/src/components/FormularioLeads.jsx
+++ frontend/src/components/FormularioLeads.jsx
@@ -7,7 +7,6 @@ export function FormularioLeads({carregarLeads}) {
     const [email, setEmail] = useState("");
     const [whatsapp, setWhatsapp] = useState("");
 
-
 async function handleSubmit(e) {
   try{  
   e.preventDefault();
@@ -24,6 +23,18 @@ async function handleSubmit(e) {
   }
 
 }
+
+function formatWhastApp(e) {
+  let value = e.target.value.replace(/\D/g, ""); // Remove tudo que não é número 
+   // Aplica a máscara apenas se houver números
+  if (value.length > 0) {
+   
+    value = value.replace(/^(\d{2})(\d)/g, "($1) $2"); // Formata o DDD: (XX)
+    value = value.replace(/(\d{5})(\d)/, "$1-$2"); // Formata o número: (XX) XXXXX-XXXX
+  }
+  setWhatsapp(value.slice(0, 15));// Limita a 15 caracteres: (99) 99999-9999
+};
+
   
 return (
   
@@ -40,7 +51,7 @@ return (
         />
         <input 
           className="bg-slate-800 border border-slate-700 p-2.5 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
-          placeholder="WhatsApp" value={whatsapp} onChange={e => setWhatsapp(e.target.value)} required 
+          placeholder="(00) 00000-0000" value={whatsapp} onChange={formatWhastApp} required 
         />
         <button className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2.5 rounded-lg transition-colors cursor-pointer">
           Cadastrar Lead

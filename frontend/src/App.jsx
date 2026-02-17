9a9db55e2290f121300389cad359257a4bc9a1f4
Wellinton Rodrigo Silva <wellinton.rodrigo26@gmail.com>
2026-02-17 10:34:23 -0300

    feat: Implementado a proteção na rota Dashboard
    Criado componente PrivateRoute para restringir o acesso ao Dashboard apenas para usuários autenticados com token no localStorage.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { CadastroUsuario } from './pages/CadastroUsuario.jsx';
 import { Dashboard } from './pages/Dashboard.jsx';
-
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 function App() {
+  
+
 
 return(
 <BrowserRouter>
@@ -12,7 +14,14 @@ return(
     <Route path='/' element={<Login />} />
     <Route path='/login' element={<Login />} />
     <Route path='/register' element={<CadastroUsuario />} />
-    <Route path='/dashboard' element={<Dashboard />} />
+    <Route 
+  path='/dashboard' 
+  element={
+    <PrivateRoute>
+      <Dashboard />
+    </PrivateRoute>
+  } 
+/>
   </Routes>
 </BrowserRouter>
 )};
